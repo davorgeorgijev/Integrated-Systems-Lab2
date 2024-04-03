4ca5edd6875bffcf17370d85bf9a0eb7feda7689
Davor Georgijev <davorgeorgijev@hotmail.com>
2024-04-03 20:12:10 +0200

    Initial commit

---
+++ BookingApplication/BookingApplication/Models/BookingApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookingApplication.Models
+{
+    public class BookingApplicationUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public virtual ICollection<Reservation>? Reservations { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual BookingList? BookingList { get; set; }
+
+    }
+}
